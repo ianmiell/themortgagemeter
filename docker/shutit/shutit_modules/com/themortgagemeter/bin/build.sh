077e4a9368c0d9048efdab92d79db5c799885459
Ian Miell <ian.miell@gmail.com>
2015-02-09 11:09:04 +0000

    non-interactive

--- docker/shutit/shutit_modules/com/themortgagemeter/bin/build.sh
+++ docker/shutit/shutit_modules/com/themortgagemeter/bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd ..
-shutit build --shutit_module_path $(dirname $(which shutit))/library --image_tag stackbrew/ubuntu:saucy
+shutit build --shutit_module_path $(dirname $(which shutit))/library --image_tag stackbrew/ubuntu:saucy --interactive 0
 if [[ "x$?" != "x0" ]]
 then
 	popd
