2747a936ef14fb5b5fc6005cba2a599abf43516f
Ian Miell <ian.miell@gmail.com>
2015-05-04 16:59:44 +0100

    upgrades to django

--- website/django/themortgagemeter/wsgi-scripts/wsgi.py
+++ website/django/themortgagemeter/wsgi-scripts/wsgi.py
@@ -3,6 +3,7 @@ import sys
 
 # This file needs to be at this level of the hierarchy so that relative links work when running using test server and live.
 sys.path.append('/opt/themortgagemeter/website/django/themortgagemeter')
+sys.path.append('/opt/themortgagemeter/shared')
 #sys.path.append('..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import django.core.handlers.wsgi
